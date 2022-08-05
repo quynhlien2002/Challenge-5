dafcae6345215e4ac833dcbc86e5793a92a14c33
Daisy Lien <daisylien1211@gmail.com>
2022-08-05 16:12:44 -0700

    change

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
 var today = moment();
 $('#currentDay').text(today.format("MMMM Do, YYYY"));
+// time in the heading 
 
 var colorTime = parseInt(document.querySelector('.time').textContent);
-var timeNow = parseInt(moment().format('LT'));
-// parseInt
+var timeNow = parseInt(moment().format('H'));
+// set the time in the planner and the current time 
 
-console.log(colorTime);
-console.log(timeNow);
+// console.log(colorTime);
+// console.log(timeNow);
 
-if (timeNow === colorTime){
+for (i = 0; i < colorTime.length; i++){
+if (timeNow == colorTime){
     $('tr').addClass('present');
 } else if (timeNow < colorTime){
     $('tr').addClass('past');
 } else if (timeNow > colorTime){
     $('tr').addClass('future');
 }
+} 
+
+console.log(timeNow);
+console.log(colorTime);
 
 
