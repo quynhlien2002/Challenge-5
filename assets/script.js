3a0504fe19a660d46cd28db5edc1f7041819e2e9
Daisy Lien <daisylien1211@gmail.com>
2022-08-05 19:32:48 -0700

    change

--- assets/script.js
+++ assets/script.js
@@ -2,43 +2,33 @@ var today = moment();
 $('#currentDay').text(today.format("MMMM Do, YYYY"));
 // time in the heading 
 
-var colorTime = parseInt(document.querySelector('.time').textContent);
-var timeNow = parseInt(moment().format('H'));
-// set the time in the planner and the current time 
+var timeNow = parseInt(moment().format('hh:mm a'));
 
-// console.log(colorTime);
-// console.log(timeNow);
-
-// for (i = 0; i < 18; i++){
-// if (timeNow === colorTime){
-//     $("tr").addClass('present');
-//     $
-// } else if (timeNow < colorTime){
-//     $('tr').addClass('past');
-// } else if (timeNow > colorTime){
-//     $('tr').addClass('future');
-// }
-// } 
-
-
-$('#color').each(function(){
+$('.color').each(function(){
+    var colorTime = parseInt($(this).children(".time").text());
     if (timeNow === colorTime){
-        $(".color").addClass('present');
-        $('.color').removeClass('past');
-        $('.color').removeClass('future');
+        $(this).addClass('present');
+        $(this).removeClass('past');
+        $(this).removeClass('future');
     } else if (timeNow < colorTime){
-        $('.color').addClass('past');
-        $('.color').removeClass('present');
-        $('.color').removeClass('future');
+        $(this).addClass('past');
+        $(this).removeClass('present');
+        $(this).removeClass('future');
     } else {
-        $('.color').addClass('future');
-        $('.color').removeClass('past');
-        $('.color').removeClass('present');
+        $(this).addClass('future');
+        $(this).removeClass('past');
+        $(this).removeClass('present');
     }
+});
+
+$('.saveBtn').on('click', function(){
+    var listItem = $('.list-item').value;
+    console.log(listItem);
 })
 
-console.log(timeNow);
-console.log(colorTime);
+
+
+
 
 
 
