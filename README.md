7904172a243e92c6d4bd048ac845bdf1706acc86
Daisy Lien <daisylien1211@gmail.com>
2022-08-06 21:05:54 -0700

    change

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Challenge-5
+
+In this challenge, I am trying to make the work planner for each day. 
+
+When first load the page, it will display the date and year so you will know which day is today. 
+If the hour row is green, it's mean that this activity will be in the future, if red, which mean it is present and if grey, the hour has passed so it easier for you to know and plan out your days. 
+Then, you can log your activities in each of the text area. Once done, you can click the save button on the right side of the table and it will save your data.
+
+If for some reason you turn off the page or refresh it, your activities still show up and does not delete. If you want to change, just delete the text and then fill in your new plan for a day. 
+
+This is the example of the work planer: 
+![](./image/Screen%20Shot%202022-08-06%20at%2021.01.54.png)
--- assets/script.js
+++ assets/script.js
@@ -28,8 +28,8 @@ $('.saveBtn').on('click', function(){
     localStorage.setItem('listItemSix', listItemSix);
 
     localStorage.getItem('listItemSix');
-    if ( six !== null && six !== undefined){
-    $('.list-item-six').val(six);
+    if ( listItemSix !== null && listItemSix !== undefined){
+    $('.list-item-six').val(listItemSix);
 
 
     var listItemSeven = $('.list-item-seven').val();
@@ -49,10 +49,166 @@ $('.saveBtn').on('click', function(){
     localStorage.getItem('listItemEight');
     if ( listItemEight !== null){
     $('.list-item-eight').val(listItemEight);
+    }
+
+    var listItemNine = $('.list-item-nine').val();
+    console.log(listItemNine);
+    localStorage.setItem('listItemNine', listItemNine);
+
+    localStorage.getItem('listItemNine');
+    if ( listItemNine !== null){
+    $('.list-item-nine').val(listItemNine);
+    }
+
+    var listItemTen = $('.list-item-ten').val();
+    console.log(listItemTen);
+    localStorage.setItem('listItemTen', listItemTen);
+
+    localStorage.getItem('listItemTen');
+    if ( listItemTen !== null){
+    $('.list-item-ten').val(listItemTen);
+    }
+
+    var listItemEleven = $('.list-item-eleven').val();
+    console.log(listItemEleven);
+    localStorage.setItem('listItemElevent', listItemEleven);
+
+    localStorage.getItem('listItemElevent');
+    if ( listItemEleven !== null){
+    $('.list-item-eleven').val(listItemEleven);
+    }
+
+    var listItemTwelve = $('.list-item-twelve').val();
+    console.log(listItemTwelve);
+    localStorage.setItem('listItemTwelve', listItemTwelve);
+
+    localStorage.getItem('listItemTwelve');
+    if ( listItemTwelve !== null){
+    $('.list-item-twelve').val(listItemTwelve);
+    }
+
+    var listItemThirteen = $('.list-item-thirteen').val();
+    console.log(listItemThirteen);
+    localStorage.setItem('listItemThirteen', listItemThirteen);
+
+    localStorage.getItem('listItemThirteen');
+    if ( listItemThirteen !== null){
+    $('.list-item-thirteen').val(listItemThirteen);
+    }
+
+    var listItemFourteen = $('.list-item-foourteen').val();
+    console.log(listItemFourteen);
+    localStorage.setItem('listItemFourteen', listItemFourteen);
+
+    localStorage.getItem('listItemFourteen');
+    if ( listItemFourteen !== null){
+    $('.list-item-fourteen').val(listItemFourteen);
+    }
+
+    var listItemFifteen = $('.list-item-fifteen').val();
+    console.log(listItemFifteen);
+    localStorage.setItem('listItemFifteen', listItemFifteen);
+
+    localStorage.getItem('listItemFifteen');
+    if ( listItemFifteen !== null){
+    $('.list-item-fifteen').val(listItemFifteen);
+    }
+
+    var listItemSixteen = $('.list-item-sixteen').val();
+    console.log(listItemSixteen);
+    localStorage.setItem('listItemSixteen', listItemSixteen);
+
+    localStorage.getItem('listItemSixteen');
+    if ( listItemSixteen !== null){
+    $('.list-item-sixteen').val(listItemSixteen);
+    }
+
+    var listItemSeventeen = $('.list-item-seventeen').val();
+    console.log(listItemSeventeen);
+    localStorage.setItem('listItemSeventeen', listItemSeventeen);
+
+    localStorage.getItem('listItemSeventeen');
+    if ( listItemSeventeen !== null){
+    $('.list-item-seventeen').val(listItemSeventeen);
+    }
+
+    var listItemEighteen = $('.list-item-eighteen').val();
+    console.log(listItemEighteen);
+    localStorage.setItem('listItemEighteen', listItemEighteen);
+
+    localStorage.getItem('listItemEighteen');
+    if ( listItemEighteen !== null){
+    $('.list-item-eighteen').val(listItemEighteen);
     
 } 
 });
 
+$('.saveBtn').ready(function(){
+    var listItemSix = localStorage.getItem('listItemSix');
+    if ( listItemSix !== null && listItemSix !== undefined){
+    $('.list-item-six').val(listItemSix);
+    }
+
+    var listItemSeven = localStorage.getItem('listItemSeven');
+    if ( listItemSeven !== null && listItemSeven !== undefined){
+    $('.list-item-seven').val(listItemSeven);
+    };
+
+    var listItemEight = localStorage.getItem('listItemEight');
+    if ( listItemEight !== null){
+    $('.list-item-eight').val(listItemEight);
+    }
+
+    var listItemNine = localStorage.getItem('listItemNine');
+    if ( listItemNine !== null){
+    $('.list-item-nine').val(listItemNine);
+    }
+
+    var listItemTen = localStorage.getItem('listItemTen');
+    if ( listItemTen !== null){
+    $('.list-item-ten').val(listItemTen);
+    }
+
+    var listItemEleven = localStorage.getItem('listItemElevent');
+    if ( listItemEleven !== null){
+    $('.list-item-eleven').val(listItemEleven);
+    }
+
+    var listItemTwelve = localStorage.getItem('listItemTwelve');
+    if ( listItemTwelve !== null){
+    $('.list-item-twelve').val(listItemTwelve);
+    }
+
+    var listItemThirteen = localStorage.getItem('listItemThirteen');
+    if ( listItemThirteen !== null){
+    $('.list-item-thirteen').val(listItemThirteen);
+    }
+
+    var listItemFourteen = localStorage.getItem('listItemFourteen');
+    if ( listItemFourteen !== null){
+    $('.list-item-fourteen').val(listItemFourteen);
+    }
+
+    var listItemFifteen = localStorage.getItem('listItemFifteen');
+    if ( listItemFifteen !== null){
+    $('.list-item-fifteen').val(listItemFifteen);
+    }
+
+    var listItemSixteen = localStorage.getItem('listItemSixteen');
+    if ( listItemSixteen !== null){
+    $('.list-item-sixteen').val(listItemSixteen);
+    }
+
+    var listItemSeventeen = localStorage.getItem('listItemSeventeen');
+    if ( listItemSeventeen !== null){
+    $('.list-item-seventeen').val(listItemSeventeen);
+    }
+
+    var listItemEighteen = localStorage.getItem('listItemEighteen');
+    if ( listItemEighteen !== null){
+    $('.list-item-eighteen').val(listItemEighteen);
+    } 
+});
 
 
 
