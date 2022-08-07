35e01c3ba721c90a8fc78042b688cc54b4b73fce
Daisy Lien <daisylien1211@gmail.com>
2022-08-06 19:01:25 -0700

    change

--- assets/script.js
+++ assets/script.js
@@ -25,35 +25,28 @@ $('.color').each(function(){
 $('.saveBtn').on('click', function(){
     var listItemSix = $(this).siblings('td').children('.list-item-six').val();
     console.log(listItemSix);
-    localStorage.setItem('listItemSix', listItemSix)
-});
-
-var listItemSix = localStorage.getItem('listItemSix')
-if ( listItemSix !== null && listItemSix !== undefined){
+    localStorage.setItem('listItemSix', listItemSix);
+    localStorage.getItem('listItemSix');
+    if ( listItemSix !== null && listItemSix !== undefined){
     $('.list-item-six').val(listItemSix);
-}
+    };
 
-$('.saveBtn').on('click', function(){
     var listItemSeven = $(this).siblings('td').children('.list-item-seven').val();
     console.log(listItemSeven);
-    localStorage.setItem('listItemSeven', listItemSeven)
-});
-
-var listItemSeven = localStorage.getItem('listItemSeven')
-if ( listItemSeven !== null && listItemSeven !== undefined){
+    localStorage.setItem('listItemSeven', listItemSeven);
+    localStorage.getItem('listItemSeven');
+    if ( listItemSeven !== null && listItemSeven !== undefined){
     $('.list-item-seven').val(listItemSeven);
-} 
+    };
 
-$('.saveBtn').on('click', function(){
     var listItemEight = $(this).siblings('td').children('.list-item-eight').val();
     console.log(listItemEight);
-    localStorage.setItem('listItemEight', listItemEight)
-});
-
-var listItemEight = localStorage.getItem('listItemEight')
-if ( listItemEight !== null){
+    localStorage.setItem('listItemEight', listItemEight);
+    localStorage.getItem('listItemEight');
+    if ( listItemEight !== null){
     $('.list-item-eight').val(listItemEight);
 } 
+});
 
 
 
